dd3a3ca405b3e0ef2b163f5341179dca57dc7713
DeePSiDeinside <69675853+DeePSiDeinside@users.noreply.github.com>
2022-11-09 12:53:05 +0530

    Update jsAttempt.js

--- jsAttempt.js
+++ jsAttempt.js
@@ -9,24 +9,80 @@ const arrowPin = 11; //arrow area
 const blockPin = 12; //block area
 const upPin = 13; //move up in block area
 
+// rest 0
+//moving 1
+//turning 2
+
+class Commands {
+  constructor(){
+    this.elements = [];
+  }
+  
+  add(comm, data){
+    //commm 0 = stop, 1 = move, 2 = rotate
+    this.elements.push([comm, data])
+  }
+  
+  getLength(){
+    return this.elements.length
+  }
+  
+  fetch(){
+    return this.elements.shift()
+  }
+}
+
+const commands = new Commands();
+
+let state = 0
+
 setSpeed(255);
 set(15, setInterval(loop, 100));
 
 function turn(deg) {
-  newRotation = ((get(rotationPin)*1023/5) + deg)%360
-  set(stopPin, 0)
-  setSpeed(50)
-  set(stopPin, 1)
-  set(rightPin, 1)
-  while (get(rotationPin)*1023/5 != newRotation){
-    console.log("Turning")
-  }
-  set(rightPin, 0)
+  commands.add(2, deg)
 }
 
 function loop() {
+
+  turn(90)
+  console.log("Turned")
+  // if (state == 2){
+  //   setSpeed(50)
+  //   set(stopPin, 1)
+  //   set(rightPin, 1)
+  // }
+  
+  if (commands.getLength() > 0) {
+    var curCommand = commands.fetch()
+    console.log(curCommand)
+    if (curCommand[0] == 2){
+      deg = curCommand[1]
+      newRotation = ((get(rotationPin)*1023/5) + deg)%360
+      setSpeed(50)
+      set(stopPin, 1)
+      set(rightPin, 1)
+      
+      var turned = false
+      
+      var turning = setInterval(() => {
+        if (get(rotationPin)*1023/5 < newRotation+10 || get(rotationPin)*1023/5 > newRotation-10){
+          clearInterval(turning)
+        }else{
+          console.log("Turning")
+        }
+      })
+
+      set(rightPin, 0)
+      set(stopPin, 0)
+      setSpeed(255)
+    }
+    
+  }
+      
   
 }
+
 
 function set(pin, value) {
   document.getElementById(pin + "out").textContent = value;
